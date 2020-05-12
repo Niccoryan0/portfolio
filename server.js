893f6450a87fbe8b0ebbe511327e6858d1a3e2bf
Niccoryan0 <niccoryan0@gmail.com>
2020-05-11 21:54:16 -0700

    removed nucessary code

--- server.js
+++ server.js
@@ -8,19 +8,4 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-app.get('/hello', (request, response) => {
-  response.status(200).send('Hello');
-});
-
-app.get('/data', (request, response) => {
-  let airplanes = {
-    departure: Date.now(),
-    canFly: true,
-    pilot: 'Well Trained',
-  };
-  response.status(200).json(airplanes);
-});
-
-app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
-
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
